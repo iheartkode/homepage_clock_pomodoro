072b88eba676c88f40cfddb87a61c2810b97ec07
iheartkode <markhjr@outlook.com>
2015-11-28 22:07:30 -0800

    added notifs for break

--- app.js
+++ app.js
@@ -13,14 +13,28 @@ function notifyMeWork() {
   if (Notification.permission !== "granted")
     Notification.requestPermission();
   else {
-    var notification = new Notification('Notification title', {
-      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-      body: "Hey there! You've been notified!",
+    var notification = new Notification('Working', {
+      body: "Hey there! You have worked 25 minutes",
     });
 
-    notification.onclick = function () {
-      window.open("http://stackoverflow.com/a/13328397/1269037");
-    };
+  }
+
+}
+
+function notifyMeBreak() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.');
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Break Time', {
+
+      body: "Hey there! Break time is up!",
+    });
+
 
   }
 
@@ -51,6 +65,7 @@ function work() {
 function breakTime() {
   $("#output").text("Timer is set for  5 Minutes enjoy your break!");
   setTimeout(function() {
+    notifyMeBreak();
     var audio = new Audio('break.mp3');
     audio.play();
       $("#output").text("Your break is over, get to work!");
